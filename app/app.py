ca72a15498d78eadc44912be153ab153ab825930
MugeraH <bwosihughes24@gmail.com>
2025-11-08 07:09:18 +0300

    feat: integrate ImageKit for file uploads
    - Implemented actual file upload to ImageKit CDN replacing dummy placeholder logic
    - Added temporary file handling with proper cleanup to manage upload process
    - Enhanced file type detection to distinguish between video and image content

--- app/app.py
+++ app/app.py
@@ -4,6 +4,12 @@ from app.db import Post,create_db_and_tables,get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
+from app.images import imagekit
+from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
+import os
+import shutil
+import uuid
+import tempfile
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
@@ -19,17 +25,45 @@ async def upload_file(
     session:AsyncSession = Depends(get_async_session)
 
 ):
-    post = Post(
-        caption=caption,
-        url="dummy url",
-        file_type="photo",
-        file_name="dummy name"
+    temp_file_path=None
+    try:
+        with tempfile.NamedTemporaryFile(delete=False,suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path=temp_file.name
+            shutil.copyfileobj(file.file,temp_file)
+        upload_result=imagekit.upload_file(
+            file=open(temp_file_path,"rb"),
+            file_name=file.filename,
+            options=UploadFileRequestOptions(
+                use_unique_file_name=True,
+                tags=["backend-upload"]
+            )
+        )
+        file_name=upload_result.file_name or upload_result.name or file.filename
+     
+        if upload_result.response_metadata.http_status_code == 200:
+            post = Post(
+                caption=caption,
+                url=upload_result.url,
+                file_type="video" if file.content_type.startswith("video/") else "image",
+                file_name=file_name
 
-    )
-    session.add(post)
-    await session.commit()
-    await session.refresh(post)
-    return post
+            )
+            session.add(post)
+            await session.commit()
+            await session.refresh(post)
+            return post
+
+
+    except Exception as e:
+       raise HTTPException(status_code=500,detail=str(e))
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        file.file.close()
+        
+
+
+   
 
 
 @app.get("/feed")
